f10bb286046ee9b6a266f55588bff6dcf9e1bd03
Jackson Davis <davisj564@gmail.com>
2020-12-04 12:00:41 -0500

    finalized all css

--- front/src/components/Final.js
+++ front/src/components/Final.js
@@ -4,7 +4,7 @@ import '../App.css';
 const Final = (props) => {
     return (
         <div className="content">
-            <h1 className="finalTitle" id="finalTitle">Your Final Song is: {props.finalSong.Title}</h1>
+            <h1 className="finalTitle" id="finalTitle">LISTEN TO: {props.finalSong.Title}</h1>
             <img src={props.finalSong.Pic} className="finalPicture"></img>
             <p className="finalText">{props.finalSong.Link}</p>
         </div >
--- front/src/components/Quiz.js
+++ front/src/components/Quiz.js
@@ -4,18 +4,14 @@ import '../App.css';
 const Quiz = (props) => {
     return (
         <div className="content">
-            <div className="header">
-                <h1>Carti Season</h1>
-            </div>
-            <button className="rstButton" id="rstBtn" onClick={props.reset}>reset</button>
+            <button className="rstButton" id="rstBtn" onClick={props.reset}>RESET</button>
             <h1 className="title">
                 {props.question}
             </h1>
             <button className="buttons" id="button1" onClick={() => props.finalAnswer(props.choice1)}>{props.choice1}</button>
             <button className="buttons" id="button2" onClick={() => props.finalAnswer(props.choice2)}>{props.choice2}</button>
             <button className="buttons" id="button3" onClick={() => props.finalAnswer(props.choice3)}>{props.choice3}</button>
-            <button className="buttons" id="nxtBtn" onClick={props.counter}>next</button>
-
+            <button className="buttons" id="nxtBtn" onClick={props.counter}>NEXT</button>
         </div >
     )
 }
