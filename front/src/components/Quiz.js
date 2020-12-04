367820c13058daca137d2ebd6228d09b02459cc4
Jackson Davis <davisj564@gmail.com>
2020-12-03 19:07:07 -0500

    front end essentially done except for styling

--- front/src/components/Quiz.js
+++ front/src/components/Quiz.js
@@ -9,8 +9,8 @@ const Quiz = (props) => {
             <button id="button1" onClick={() => props.finalAnswer(props.choice1)}>{props.choice1}</button>
             <button id="button2" onClick={() => props.finalAnswer(props.choice2)}>{props.choice2}</button>
             <button id="button3" onClick={() => props.finalAnswer(props.choice3)}>{props.choice3}</button>
-            <button onClick={props.counter}>next</button>
-            <button onClick={props.reset}>reset</button>
+            <button id="nxtBtn" onClick={props.counter}>next</button>
+            <button id="rstBtn" onClick={props.reset}>reset</button>
         </div >
     )
 }
